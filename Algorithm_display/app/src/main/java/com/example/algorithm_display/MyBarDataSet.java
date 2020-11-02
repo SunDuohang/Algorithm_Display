714eef702320ba42e94e149445cdb8fa3e9d5cab
SunDuohang <45425312+SunDuohang@users.noreply.github.com>
2020-11-02 19:41:27 +0800

    Create MyBarDataSet.java

---
+++ Algorithm_display/app/src/main/java/com/example/algorithm_display/MyBarDataSet.java
@@ -0,0 +1,29 @@
+package com.example.algorithm_display;
+
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.List;
+
+public class MyBarDataSet extends BarDataSet {
+    float target = 0;
+    public MyBarDataSet(List<BarEntry> yVals, String label) {
+        super(yVals, label);
+    }
+    public MyBarDataSet(List<BarEntry> yVals, String label, float target1) {
+        super(yVals,label);
+        target = target1;
+    }
+
+    @Override
+    public int getColor(int index) {
+        //根据getEntryForXIndex(index).getVal()获得Y值,然后去对比,判断
+        //我这1000 4000是根据自己的需求写的,可以随便设,判断条件if根据自己需求
+        if(getEntryForIndex(index).getY() > 5)
+            return mColors.get(0);
+        else if(getEntryForIndex(index).getY() > 10)
+            return mColors.get(1);
+        else // greater or equal than 100 red
+            return mColors.get(2);
+    }
+}
