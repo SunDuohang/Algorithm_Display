29bd851f7540d2e15a0f8142b0d9d072814b4f55
SunDuohang <45425312+SunDuohang@users.noreply.github.com>
2020-11-03 15:09:28 +0800

    Update Button_Sel_Activity.java
    second time

--- Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Sel_Activity.java
+++ Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Sel_Activity.java
@@ -1,84 +1,150 @@
 package com.example.algorithm_display;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
+//选择排序
 public class Button_Sel_Activity extends AppCompatActivity {
-    private BarChart SelSort_Chart;
+    private Button btn_Ins_Ex;
+    private BarChart Sel_BarChart;
+    private  float array[] = new float[11];
+    private BarChartManager barChartManager1 ;
+    private ArrayList<Float> xValues = new ArrayList<>();
+    private List<String> names = new ArrayList<>();
+    private List<Integer> colors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button__sel_);
+        Sel_BarChart = (BarChart) findViewById(R.id.SelSort_Chart);//找到控件SelSort_Chart
 
-        SelSort_Chart = (BarChart) findViewById(R.id.SelSort_Chart);
-        SelSort_Chart = initBarChart(SelSort_Chart);
-        BarData barData = setBarData(); //这里调用方法初始化模拟数据
-        SelSort_Chart.setData(barData); //这里将模拟数据用于柱状图
-        SelSort_Chart.invalidate();     //这里让柱状图在数据填充后刷新
-        //
+        barChartManager1 = new BarChartManager(Sel_BarChart);
+        //设置x轴的数据
+        ArrayList<String> xValues0 = new ArrayList<>();
+        //设置x轴的数据
+        xValues = new ArrayList<>();
+        for (int i = 1; i < 12; i++) {
+            xValues.add((float) i);
+        }
+        //设置y轴的数据()
+        final List<List<Float>> yValues = new ArrayList<>();
+        List<Float> yValue = new ArrayList<>();
+        for (int i = 0; i < 11; i++){
+            array[i] = ((float) (Math.random() * 8) + 2);
+            yValue.add(array[i]);
+        }
+        yValues.add(yValue);
+        //颜色集合
+
+        colors.add(Color.CYAN);
+        colors.add(Color.RED);
+
+        //线的名字集合
+        names.add("柱状一");
+        names.add("柱状二");
+        //创建多条柱状的图表
+        barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors,0,array[0]);
+
+        btn_Ins_Ex = (Button) findViewById(R.id.btn_Sel_Ex);
+        btn_Ins_Ex.setOnClickListener(new View.OnClickListener() {
+            int count = -1;
+
+            public void onClick(View view) {
+
+                if(isSorted(array))
+                    Toast.makeText(Button_Sel_Activity.this, "排序已完成", Toast.LENGTH_SHORT).show();
+                else {
+                    count++;
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            try {
+                                Thread.sleep(1000);//休眠1000毫秒
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            /**
+                             * 要执行的操作
+                             */
+                            int lowest_value_index = count;
+                            for (int j = count + 1; j < array.length; j++) {
+                                if (array[j] < array[lowest_value_index]) {
+                                    lowest_value_index = j;
+                                }
+                            }
+                            List<Float> yValue = new ArrayList<>();
+                            for (int k = 0; k < array.length; k++) {
+                                yValue.add(array[k]);
+                            }
+                            yValues.add(yValue);
+                            barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors, lowest_value_index, array[lowest_value_index]);
+                            final int finalLowest_value_index = lowest_value_index;
+                            new Thread() {
+                                @Override
+                                public void run() {
+                                    super.run();
+                                    try {
+                                        Thread.sleep(500);//休眠1秒
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    /**
+                                     * 要执行的操作
+                                     */
+                                    float temp_value = array[count];
+                                    array[count] = array[finalLowest_value_index];
+                                    array[finalLowest_value_index] = temp_value;
+                                    yValues.clear();
+                                    List<Float> yValue = new ArrayList<>();
+                                    for (int k = 0; k < array.length; k++) {
+                                        yValue.add(array[k]);
+                                    }
+                                    yValues.add(yValue);
+                                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors, count, array[count]);
+                                }
+                            }.start();
+                        }
+                    }.start();
+                }
+
+            }
+        });
     }
-
-    //该方法定义一个柱状图数据；
-    public BarData setBarData(){
-        List<BarEntry> entries = new ArrayList<>();  //定义一个存放数据的容器
-        //这里用for循环为容器写入数据
-        //填充12个200以内的随机数
-        entries.add(new BarEntry(0, new Random().nextInt(200)));
-        entries.add(new BarEntry(1, new Random().nextInt(200)));
-        entries.add(new BarEntry(2, new Random().nextInt(200)));
-        entries.add(new BarEntry(3, new Random().nextInt(200)));
-        entries.add(new BarEntry(4, new Random().nextInt(200)));
-        entries.add(new BarEntry(5, new Random().nextInt(200)));
-        BarDataSet barDataSet = new BarDataSet(entries,"测试用数据");
-        BarData barData = new BarData(barDataSet);
-        return barData;   //返回可用于柱状图的数据
-    }
-
-    //初始化柱状图；
-    public BarChart initBarChart(BarChart barChart){
-        barChart.setDescription(null);    //设置柱状图解释性文字为空
-        barChart.setDrawBarShadow(false); //设置柱状图每条柱子的阴影不显示
-        barChart.setDrawValueAboveBar(true); //设置柱状图每一条柱子的数值显示
-        XAxis xAxis = barChart.getXAxis();   //获取柱状图的x轴
-        YAxis yAxisLeft = barChart.getAxisLeft();//获取柱状图左侧的y轴
-        YAxis yAxisRight = barChart.getAxisRight();
-        setAxis(xAxis,yAxisLeft,yAxisRight);            //调用方法设置柱状图轴线
-        return barChart;      //返回初始化完成的柱状图实例
-    }
-
-    //设置柱状图的x轴与y轴；
-    public void setAxis(XAxis xAxis, YAxis yAxisLeft,YAxis yAxisRight){
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//x轴在柱状图底部显示
-        xAxis.setAxisLineWidth(1);    //设置x轴宽度
-        xAxis.setAxisMaximum(0);//设置x轴从0刻度开始绘画
-        xAxis.setDrawAxisLine(true);//这里设置x轴显示轴线
-        xAxis.setGranularity(1);
-        xAxis.setDrawGridLines(false);//这里设置x轴的表格线不显示
-        xAxis.setEnabled(true);//设置x轴显示
-
-        yAxisLeft.setAxisLineWidth(1);
-        yAxisLeft.setAxisMinimum(0);
-        yAxisLeft.setDrawGridLines(false);
-        yAxisLeft.setDrawAxisLine(true);
-        yAxisLeft.setEnabled(true);
-
-        yAxisRight.setAxisLineWidth(1);
-        yAxisRight.setAxisMinimum(0);
-        yAxisRight.setDrawGridLines(false);
-        yAxisRight.setDrawAxisLine(true);
-        yAxisRight.setEnabled(false);
+    public static boolean isSorted(float[] data) {
+        boolean flag1 = false,flag2 = false;
+        //数组是否为升序
+        for (int i = 0; i < data.length-1; i++) {
+            if (data[i] == Math.min(data[i], data[i + 1])) {
+                flag1 = true;
+            } else {
+                flag1 = false;
+                break;
+            }
+        }
+        //数组是否为降序
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] == Math.max(data[i], data[i + 1])) {
+                flag2 = true;
+            } else {
+                flag2 = false;
+                break;
+            }
+        }
+        if (flag1 || flag2) {
+            return true;//有一个为真，即是已经排过序了
+        } else {
+            return  false;
+        }
     }
 }
