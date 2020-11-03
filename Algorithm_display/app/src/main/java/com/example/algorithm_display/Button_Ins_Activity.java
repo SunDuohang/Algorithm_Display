f03c46338a36a3538c26fcbefcd0eaa35a9b6175
SunDuohang <45425312+SunDuohang@users.noreply.github.com>
2020-11-03 15:09:13 +0800

    Update Button_Ins_Activity.java
    second time

--- Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Ins_Activity.java
+++ Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Ins_Activity.java
@@ -12,12 +12,12 @@ import com.github.mikephil.charting.charts.BarChart;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//插入排序
 public class Button_Ins_Activity extends AppCompatActivity {
 
     private Button btn_Ins_Ex;
-    private BarChart barChart1;
-    private  float array[] = new float[5];
+    private BarChart Ins_BarChart;
+    private  float array[] = new float[11];
     private BarChartManager barChartManager1 ;
     private ArrayList<Float> xValues = new ArrayList<>();
     private List<String> names = new ArrayList<>();
@@ -26,34 +26,33 @@ public class Button_Ins_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button__ins_);
-        barChart1 = findViewById(R.id.InsSort_Chart);
-        barChartManager1 = new BarChartManager(barChart1);
+        Ins_BarChart = findViewById(R.id.InsSort_Chart);
+        barChartManager1 = new BarChartManager(Ins_BarChart);
         //设置x轴的数据
         ArrayList<String> xValues0 = new ArrayList<>();
         //设置x轴的数据
         xValues = new ArrayList<>();
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 12; i++) {
             xValues.add((float) i);
         }
         //设置y轴的数据()
         final List<List<Float>> yValues = new ArrayList<>();
         List<Float> yValue = new ArrayList<>();
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 11; i++){
             array[i] = ((float) (Math.random() * 8) + 2);
             yValue.add(array[i]);
         }
         yValues.add(yValue);
         //颜色集合
 
-        colors.add(Color.GREEN);
-        colors.add(Color.BLUE);
-        colors.add(Color.RED);
         colors.add(Color.CYAN);
+        colors.add(Color.RED);
+
         //线的名字集合
         names.add("柱状一");
         names.add("柱状二");
         //创建多条柱状的图表
-        barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors);
+        barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors,0,array[0]);
 
         btn_Ins_Ex = (Button) findViewById(R.id.btn_Ins_Ex);
         btn_Ins_Ex.setOnClickListener(new View.OnClickListener() {
@@ -61,7 +60,12 @@ public class Button_Ins_Activity extends AppCompatActivity {
 
             public void onClick(View view) {
                 count++;
-                Toast.makeText(Button_Ins_Activity.this, "第"+count+"次遍历",Toast.LENGTH_SHORT).show();
+                if(count < array.length) {
+                    Toast.makeText(Button_Ins_Activity.this, "第" + count + "次遍历", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(Button_Ins_Activity.this, "排序已经完成！", Toast.LENGTH_SHORT).show();
+
                 if(count>0&&count<array.length){
                     float currentValue = array[count];
                     int position = count;
@@ -80,7 +84,7 @@ public class Button_Ins_Activity extends AppCompatActivity {
                         yValue.add(array[j]);
                     }
                     yValues.add(yValue);
-                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors, count);
+                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors, count,array[count]);
                 }
             }
         });
