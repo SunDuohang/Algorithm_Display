51af178d527d6105c6ab5f16efe537fba68ab1df
SunDuohang <45425312+SunDuohang@users.noreply.github.com>
2020-11-03 15:09:20 +0800

    Update Button_Quick_Activity.java
    second time

--- Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Quick_Activity.java
+++ Algorithm_display/app/src/main/java/com/example/algorithm_display/Button_Quick_Activity.java
@@ -1,117 +1,179 @@
 package com.example.algorithm_display;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//快速排序
 public class Button_Quick_Activity extends AppCompatActivity {
-    private BarChart chart;
-    private List<BarEntry> list = new ArrayList<>();
-    private CountDownTimer count;
+    private Button btn_Quick_Ex;
+    private BarChart Quick_BarChart;
+    private  float array[] = new float[11];
+    private BarChartManager barChartManager1 ;
+    private ArrayList<Float> xValues = new ArrayList<>();
+    private List<String> names = new ArrayList<>();
+    private List<Integer> colors = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button__quick_);
-        chart = findViewById(R.id.QuickSort_Chart);
-        info();
-    }
+        Quick_BarChart = findViewById(R.id.QuickSort_Chart);//找到控件QuickSort_Chart
 
-
-    private void info() {
-        XAxis xAxis = chart.getXAxis();
-        //取消x轴网格线
-        xAxis.setDrawGridLines(false);
-        //设置x轴位置
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        //设置x轴显示标签数
-        xAxis.setLabelCount(25);
-        //定义x轴最大值
-        xAxis.setAxisMaximum(25f);
-        //禁用图表右边Y轴
-        chart.getAxisRight().setEnabled(false);
-        //取消图表左边y轴网格线
-        chart.getAxisLeft().setDrawGridLines(false);
-
-        //x轴标签集合,i的大小与自定义的最大值关联。
-        final List<String> list = new ArrayList<>();
-        for (int i = 0; i < 20; i ++) {
-            list.add("" + i);
+        barChartManager1 = new BarChartManager(Quick_BarChart);
+        //设置x轴的数据
+        ArrayList<String> xValues0 = new ArrayList<>();
+        //设置x轴的数据
+        xValues = new ArrayList<>();
+        for (int i = 1; i < 12; i++) {
+            xValues.add((float) i);
         }
-        //自定义x轴标签
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                Log.e("demo", String.valueOf(value));
-                if (value > 19) {
-                    return "";
-                } else {
-                    return list.get((int) value);
+        //设置y轴的数据()
+        final List<List<Float>> yValues = new ArrayList<>();
+        List<Float> yValue = new ArrayList<>();
+        for (int i = 0; i < 11; i++){
+            array[i] = ((float) (Math.random() * 8) + 2);
+            yValue.add(array[i]);
+        }
+        yValues.add(yValue);
+        //颜色集合
+
+        colors.add(Color.CYAN);
+        colors.add(Color.RED);
+
+        //线的名字集合
+        names.add("柱状一");
+        names.add("柱状二");
+        //创建多条柱状的图表
+        barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors,0,array[0]);
+
+        btn_Quick_Ex = (Button) findViewById(R.id.btn_Quick_Ex);
+        btn_Quick_Ex.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                if(isSorted(array))
+                    Toast.makeText(Button_Quick_Activity.this, "排序完成", Toast.LENGTH_SHORT).show();
+                else
+                    quicksort(array, 0, array.length - 1);
+                    /*yValues.clear();
+                    List<Float> yValue = new ArrayList<>();
+                    for(int j = 0; j < array.length; j++){
+                        yValue.add(array[j]);
+                    }
+                    yValues.add(yValue);
+                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors);*/
                 }
-            }
         });
-
-        //取消图例显示
-        chart.getLegend().setEnabled(false);
-
-        //取消描述显示
-        Description description = new Description();
-        description.setEnabled(false);
-        chart.setDescription(description);
-
-        //开启定时器
-        count = new CountDownTimer(Integer.MAX_VALUE, 500) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                setData((int) (Math.random() * 80));
+    }
+    private int partition(float array[], int first, int last){
+        int i = first;
+        int j = last;
+        float key = array[first];
+        while(i < j){
+            while(i < j && array[j] >= key)
+                j --;
+            if(i < j) {
+                array[i] = array[j];
+                i ++;
             }
+            //交换一次值以后重新显示一次Chart
 
-            @Override
-            public void onFinish() {
-
+            while (array[i] < key && i < j)
+                i ++;
+            if(i < j) {
+                array[j] = array[i];
+                j --;
             }
-        }.start();
-    }
+            //交换一次值以后重新显示一次Chart
 
-    private void setData(int k) {
-        //每次存储在下标为0的位置
-        list.add(0, new BarEntry(0, k));
-        //更改x轴的下标
-        for (int i = 0; i < list.size(); i++) {
-            list.get(i).setX(i);
         }
-        //条目大于20时，删除最后的条目
-        if (list.size() > 20) {
-            list.remove(20);
-        }
-        BarDataSet set = new BarDataSet(list, "");
-        BarData data = new BarData(set);
-        data.setBarWidth(0.6f);
-        chart.setData(data);
-        //刷新布局
-        chart.invalidate();
+        array [i] = key;
+        return i;
     }
+    private void quicksort(final float array[], final int first, final int last){
+        final List<List<Float>> yValues = new ArrayList<>();
+        final List<Float> yValue = new ArrayList<>();
+        float key = array[first];
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(count!=null){
-            count.cancel();
-            count=null;
+        if(first < last){
+            final int part = partition(array ,first, last);
+            new Thread() {
+                @Override
+                public void run() {
+                    super.run();
+                    try {
+                        Thread.sleep(1500);//休眠1500毫秒
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    /**
+                     * 要执行的操作
+                     */
+                    quicksort(array,first,part-1);
+                    for(int j = 0; j < 11; j++){
+                        yValue.add(array[j]);
+                    }
+                    yValues.clear();
+                    yValues.add(yValue);
+                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors,part,array[part]);
+                }
+            }.start();
+            new Thread() {
+                @Override
+                public void run() {
+                    super.run();
+                    try {
+                        Thread.sleep(1500);//休眠1500毫秒
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    /**
+                     * 要执行的操作
+                     */
+                    quicksort(array,part+1,last);
+                    for(int j = 0; j < 11; j++){
+                        yValue.add(array[j]);
+                    }
+                    yValues.clear();
+                    yValues.add(yValue);
+                    barChartManager1.showBarChart(xValues, yValues.get(0), names.get(0), colors,part,array[part]);
+                }
+            }.start();
+
         }
     }
-
+    public static boolean isSorted(float[] data) {
+        boolean flag1 = false,flag2 = false;
+        //数组是否为升序
+        for (int i = 0; i < data.length-1; i++) {
+            if (data[i] == Math.min(data[i], data[i + 1])) {
+                flag1 = true;
+            } else {
+                flag1 = false;
+                break;
+            }
+        }
+        //数组是否为降序
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] == Math.max(data[i], data[i + 1])) {
+                flag2 = true;
+            } else {
+                flag2 = false;
+                break;
+            }
+        }
+        if (flag1 || flag2) {
+            return true;//有一个为真，即是已经排过序了
+        } else {
+            return  false;
+        }
+    }
 }
